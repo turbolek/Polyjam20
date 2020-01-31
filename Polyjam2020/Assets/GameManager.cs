be2423984c5597b81d89e52dd70d5c525b4c4f2a
Daniel Kunicki <dankunicki@gmail.com>
2020-01-31 22:30:24 +0100

    Added buttons to simulate win and lose state

---
+++ Polyjam2020/Assets/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int StepsApart;
+    public int LosingDistance;
+    public Button Player1CorrectButton;
+    public Button Player1WrongButton;
+    public Text GameOverText;
+    public Text DistanceText;
+
+    private void Start()
+    {
+        DistanceText.text = StepsApart.ToString();
+        GameOverText.enabled = false;
+        Player1CorrectButton.onClick.AddListener(DecreaseDistance);
+        Player1WrongButton.onClick.AddListener(IncreaseDistance);
+    }
+
+    private void DecreaseDistance()
+    {
+        StepsApart--;
+        DistanceText.text = StepsApart.ToString();
+        CheckEndGame();
+    }
+
+    private void IncreaseDistance()
+    {
+        StepsApart++;
+        DistanceText.text = StepsApart.ToString();
+        CheckEndGame();
+    }
+
+    private void CheckEndGame()
+    {
+        if (StepsApart <= 0)
+        {
+            WinGame();
+        }
+        else if (StepsApart >= LosingDistance)
+        {
+            LoseGame();
+        }
+    }
+
+    private void WinGame()
+    {
+        GameOverText.enabled = true;
+        GameOverText.text = "Good to be back together :)";
+    }
+
+    private void LoseGame()
+    {
+        GameOverText.enabled = true;
+        GameOverText.text = "We'll never talk to each other again :(";
+    }
+}
